61a6b14873b8888d7ab7c4779c8990310f88f93c
Sebastiaan Pouyet <sebastiaan@pouyet.nl>
2013-03-19 21:47:36 +0100

    correction

--- db/migrate/20130319202712_create_lunches.rb
+++ db/migrate/20130319202712_create_lunches.rb
@@ -2,7 +2,7 @@ class CreateLunches < ActiveRecord::Migration
   def change
     create_table :lunches do |t|
       t.integer :attendee_id
-
+      t.datetime :lunch_time
       t.timestamps
     end
   end
--- db/migrate/20130319203106_create_attendees.rb
+++ db/migrate/20130319203106_create_attendees.rb
@@ -1,11 +1,10 @@
 class CreateAttendees < ActiveRecord::Migration
   def change
     create_table :attendees do |t|
-      t.lunch :references
       t.string :emailaddress
       t.string :name
       t.integer :pluses
-
+      t.references :lunch
       t.timestamps
     end
   end
