3bd65d31939a5047ed8a380d4b5e6eea94b5bec0
Sebastiaan Pouyet <sebastiaan@pouyet.nl>
2013-03-19 21:17:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LauchCafeLunch::Application.config.secret_token = 'ec1f30eadedb63f3ab989fba90065aea736d61c8edf03ead7384251ca607ce08cf5f578a9186b42612f5bff67afa735f61471603b7e30017bf4a4092755aa095'
