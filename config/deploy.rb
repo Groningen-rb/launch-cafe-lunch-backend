e631db3414c240537e6c1d96ee24cb19bc284826
Sebastiaan Pouyet <sebastiaan@pouyet.nl>
2013-03-20 08:35:01 +0100

    added symlink task for database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,18 @@ namespace :deploy do
     db.seed
     restart
   end
+
+  before "deploy:assets:precompile" do
+    # desc "Symlinks the database.yml"
+    # task :symlink_db, :roles => :app do
+      run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    # end
+  end
+end
+
+# in RAILS_ROOT/config/deploy.rb:
+
+namespace :deploy do
 end
 
 namespace :db do
